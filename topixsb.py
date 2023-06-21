f6182ee34575a028c7a22c13bee8d27f58ec65c6
soulviper <48988221+soulviper@users.noreply.github.com>
2023-06-21 07:54:11 +0700

    Update topixsb.py

--- topixsb.py
+++ topixsb.py
@@ -1,18 +1,16 @@
 #!usr/bin/python
 
-import os, time, sys
+import os, time
+import httpx
 import random
+from colr import color
 import json
-
-#    time.sleep('3')
-#    if httpx.get('https://raw.githubusercontent.com/soulviper/tytyd/versi.txt').text != '9.1':
-#        os.unlink('topixsb.py')
-#        with open('topixsb.py', 'w') as file1:
-#            file1.write(httpx.get('https://raw.githubusercontent.com/soulviper/tytyd/topixsb.py').text)
-#        print(f"Updateing TopixSB Termux Tools {httpx.get('https://raw.githubusercontent.com/soulviper/tytyd/versi.txt').text} Version")
-#        print(f'type python topixsb.py again')
-#        print(f'ketik python topixsb.py lagi')
-#        exit()
+print(f"This Version is {'4.1'}")
+if httpx.get('https://raw.githubusercontent.com/soulviper/tytyd/versi.txt').text != '4.1':
+    with open('topixsb.py', 'w') as file1:
+        file1.write(httpx.get('https://raw.githubusercontent.com/soulviper/tytyd/topixsb.py').text)
+    print(f"Updateing TopixSB Termux Tools {httpx.get('https://raw.githubusercontent.com/soulviper/tytyd/versi.txt').text} Version")
+    exit()
 
 
 dat = {'login': False, 'nicksc': '', 'grant': '', 'header': {'Content-Type': 'application/json', 'X-Android-Package': 'com.olzhas.carparking.multyplayer', 'X-Android-Cert': 'D4962F8124C2E09A66B97C8E326AFF805489FE39', 'Accept-Language': 'in-ID, en-US', 'X-Client-Version': 'Android/Fallback/X21000008/FirebaseCore-Android', 'X-Firebase-GMPID': '1:581727203278:android:af6b7dee042c8df539459f', 'X-Firebase-Client': 'H4sIAAAAAAAAAKtWykhNLCpJSk0sKVayio7VUSpLLSrOzM9TslIyUqoFAFyivEQfAAAA', 'User-Agent': f'Dalvik/2.1.0 (Linux; U; Android 8.1.0; ASUS_X00TD MIUI/16.2017.2009.087-20{random.randint(111111, 999999)})', 'Host': 'www.googleapis.com', 'Connection': 'Keep-Alive', 'Accept-Encoding': 'gzip'}, 'idToken': 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjNmNjcyNDYxOTk4YjJiMzMyYWQ4MTY0ZTFiM2JlN2VkYTY4NDZiMzciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vY3AtbXVsdGlwbGF5ZXIiLCJhdWQiOiJjcC1tdWx0aXBsYXllciIsImF1dGhfdGltZSI6MTY2Njk2NzcxNywidXNlcl9pZCI6InIybnBPekpuaTloWXU1WWZEbWduamZUUVVhSjMiLCJzdWIiOiJyMm5wT3pKbmk5aFl1NVlmRG1nbmpmVFFVYUozIiwiaWF0IjoxNjY2OTY3NzE3LCJleHAiOjE2NjY5NzEzMTcsImVtYWlsIjoidHNiMDEwQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJ0c2IwMTBAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.gztM8gzOpNdW6PmmA_hfJDpxh8hCVC1RgAeKcBeVs5-sZnALK3gOtA85ip8khJyhC0N7i0iPwN9TsJqMGvqPb6qHGtuoe9agXWyPdqiztmKEBsJpJrvjPKJj24PmmoREnDnJq8cZHgX7BPyPoD25ZqhCC1pEkjz_XSoQF4qwLhM7-BR8BjcVYkWxxKxJPzfjMc3WfaW2ehHbYEjx8RcOQvsW6yp1zgGXYlbMavbyjHE2Zbl5ameRwic47WD_C6ugH4vtIf7XJWxbAF02CBHicJqLAkCQt0ioxtQpsYu_yq6khnNOA7vqCIn3G8kfmLIZvTNH9kQ-q7X59ePUwgvrmA', 'key': 'AIzaSyBW1ZbMiUeDZHYUO2bY8Bfnf5rRgrQGPTM', 'firebase-instance-id-token': 'fchcZJLSMpo:APA91bF8nZQY5awRdIgI41tGbAr59K6SuXEeHXC9lQiHcjNR7SN2lD4OKlQ8VuhsgJrF38NgXkDufWoDCXKz-iixYUjeNx7KildcWuQimgagDhWDMxslXhFpaQtujmqn1JywoTEvXVYZ'}
